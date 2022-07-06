3479f226f251626fa540836c37cf67871c1c3a94
TRTX-gamer <w1165577077@qq.com>
2022-07-06 17:26:24 +0800

    better

--- STM32F401xC/HAL/99_test/Core/Inc/stm32f4xx_hal_conf.h
+++ STM32F401xC/HAL/99_test/Core/Inc/stm32f4xx_hal_conf.h
@@ -64,7 +64,6 @@
 /* #define HAL_MMC_MODULE_ENABLED   */
 /* #define HAL_SPI_MODULE_ENABLED   */
 /* #define HAL_TIM_MODULE_ENABLED   */
-/* #define HAL_UART_MODULE_ENABLED   */
 /* #define HAL_USART_MODULE_ENABLED   */
 /* #define HAL_IRDA_MODULE_ENABLED   */
 /* #define HAL_SMARTCARD_MODULE_ENABLED   */
@@ -88,6 +87,7 @@
 #define HAL_FLASH_MODULE_ENABLED
 #define HAL_PWR_MODULE_ENABLED
 #define HAL_CORTEX_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
 
 /* ########################## HSE/HSI Values adaptation ##################### */
 /**
