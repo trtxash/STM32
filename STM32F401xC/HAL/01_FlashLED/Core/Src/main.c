55f9e94b2e880af1eafb70aa9a34985debfd2083
TRTX-gamer <w1165577077@qq.com>
2022-07-11 18:55:50 +0800

    选择调用user库

--- STM32F401xC/HAL/01_FlashLED/Core/Src/main.c
+++ STM32F401xC/HAL/01_FlashLED/Core/Src/main.c
@@ -9,7 +9,7 @@
  * @date 	2022年2月17号18点50分
  */
 
-//#include "delay.h"
+#include "delay.h"
 #include "sys.h"
 #include "stm32f4xx.h"
 
@@ -25,19 +25,16 @@ void LED_Init(void);
 int main(void)
 {
 	HAL_Init(); //初始化HAL库
-	// Stm32_Clock_Init(RCC_PLL_MUL9); //设置时钟,72M，因为几乎都要用时钟，最先考虑设置时钟,后面再详细学习时钟相关HAL库函数，先用
 	Stm32_Clock_Init(25, 168, 2, 4);
+	delay_init(84);               		//初始化延时函数
 	LED_Init(); //初始化LED
-	// delay_init(72);					//初始化延时函数
 
 	while (1)
 	{
 		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-		HAL_Delay(499); // 利用HAL_Delay()延迟500ms，这个函数默认加一
-		// delay_ms(500); //延时500ms
+		delay_ms(500); //延时500ms
 		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-		HAL_Delay(499); // 利用HAL_Delay()延迟500ms，这个函数默认加一
-						// delay_ms(500); //延时500ms
+		delay_ms(500); //延时500ms
 	}
 }
 
@@ -65,85 +62,3 @@ void LED_Init(void)
 
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET); // PB5置1，默认初始化后灯灭
 }
-/*----------------------------下面是正点原子的程序源码--------------------------*/
-/*
-#include "sys.h"
-#include "delay.h"
-#include "usart.h"
-
-void LED_Init(void);
-
-void LED_Init(void)
-{
-	GPIO_InitTypeDef GPIO_Initure;
-
-	__HAL_RCC_GPIOB_CLK_ENABLE();           	//开启GPIOB时钟
-	__HAL_RCC_GPIOE_CLK_ENABLE();           	//开启GPIOE时钟
-
-	GPIO_Initure.Pin=GPIO_PIN_5; 				//PB5
-	GPIO_Initure.Mode=GPIO_MODE_OUTPUT_PP;  	//推挽输出
-	GPIO_Initure.Pull=GPIO_PULLUP;          	//上拉
-	GPIO_Initure.Speed=GPIO_SPEED_FREQ_HIGH;    //高速
-	HAL_GPIO_Init(GPIOB,&GPIO_Initure);
-
-	GPIO_Initure.Pin=GPIO_PIN_5; 				//PE5
-	HAL_GPIO_Init(GPIOE,&GPIO_Initure);
-
-	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);	//PB5置1，默认初始化后灯灭
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,GPIO_PIN_SET);	//PE5置1，默认初始化后灯灭
-}
-
-int main(void) // 使用HAL库函数实现控制IO口输出
-{
-	HAL_Init();                    	 	//初始化HAL库
-	Stm32_Clock_Init(RCC_PLL_MUL9);   	//设置时钟,72M
-	delay_init(72);               		//初始化延时函数
-	LED_Init();							//初始化LED
-
-	while(1)
-	{
-		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_RESET); 	//LED0对应引脚PB5拉低，亮，等同于LED0(0)
-		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,GPIO_PIN_SET);   	//LED1对应引脚PE5拉高，灭，等同于LED1(1)
-		delay_ms(500);											//延时500ms
-		HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);   	//LED0对应引脚PB5拉高，灭，等同于LED0(1)
-		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,GPIO_PIN_RESET); 	//LED1对应引脚PE5拉低，亮，等同于LED1(0)
-		delay_ms(500);                                      	//延时500ms
-	}
-}
-
-int main(void) // 使用位带操作实现
-{
-	HAL_Init();                    	 	//初始化HAL库
-	Stm32_Clock_Init(RCC_PLL_MUL9);   	//设置时钟,72M
-	delay_init(72);               			//初始化延时函数
-	LED_Init();							//初始化LED
-
-	while(1)
-	{
-		 LED0=0;			     	//LED0亮
-		 LED1=1;				 	//LED1灭
-		 delay_ms(500);
-		 LED0=1;					//LED0灭
-		 LED1=0;					//LED1亮
-		 delay_ms(500);
-	 }
-}
-
-int main(void) // 使用直接操作结存器方式实现跑马灯
-{
-	HAL_Init();                    	 	//初始化HAL库
-	Stm32_Clock_Init(RCC_PLL_MUL9);   	//设置时钟,72M
-	delay_init(72);               		//初始化延时函数
-	LED_Init();							//初始化LED
-
-	while(1)
-	{
-	  GPIOB->BSRR=GPIO_PIN_5;     	//LED0亮
-	  GPIOE->BSRR=GPIO_PIN_5<<16; 	//LED1灭
-	  delay_ms(500);
-	  GPIOB->BSRR=GPIO_PIN_5<<16; 	//LED0灭
-	  GPIOE->BSRR=GPIO_PIN_5;     	//LED1亮
-	  delay_ms(500);
-	 }
-}
-*/
