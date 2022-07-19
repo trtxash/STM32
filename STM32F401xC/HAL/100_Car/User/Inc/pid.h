8bbcbc1503a953d7694595d7e0e4cbc2d5a2dc3d
TRTX-gamer <w1165577077@qq.com>
2022-07-19 20:17:29 +0800

    PID结构优化

--- STM32F401xC/HAL/100_Car/User/Inc/pid.h
+++ STM32F401xC/HAL/100_Car/User/Inc/pid.h
@@ -3,28 +3,7 @@
 
 #include "sys.h"
 
-// float Set_Temp1 = 60;                      //初始化P,I,D，当前值，设置值 常数
-// float PID_OUT1 = 0, PWM_Duty1 = 0;         // PID输出
-// float P_OUT1 = 0, I_OUT1 = 0, D_OUT1 = 0;  //比例输出，积分输出，微分输出
-// float Current_Error1 = 0, Last_Error1 = 0; //当前误差  最后误差
-// float Sum_Error1 = 0, Prev_Error1 = 0;     //误差积分
-// float Gain1 = 1.2, PID_I_MAX1 = 100.0, PID_I_MIN1 = -100.0, V_DATA_MAX1 = 100, V_DATA_MIN1 = 0;
-
-// float Set_Temp2 = 60;                      //初始化P,I,D，当前值，设置值 常数
-// float PID_OUT2 = 0, PWM_Duty2 = 0;         // PID输出
-// float P_OUT2 = 0, I_OUT2 = 0, D_OUT2 = 0;  //比例输出，积分输出，微分输出
-// float Current_Error2 = 0, Last_Error2 = 0; //当前误差  最后误差
-// float Sum_Error2 = 0, Prev_Error2 = 0;     //误差积分
-// float Gain2 = 1.2, PID_I_MAX2 = 100.0, PID_I_MIN2 = -100.0, V_DATA_MAX2 = 100, V_DATA_MIN2 = 0;
-
-// float Set_Temp3 = 60;                      //初始化P,I,D，当前值，设置值 常数
-// float PID_OUT3 = 0, PWM_Duty3 = 0;         // PID输出
-// float P_OUT3 = 0, I_OUT3 = 0, D_OUT3 = 0;  //比例输出，积分输出，微分输出
-// float Current_Error3 = 0, Last_Error3 = 0; //当前误差  最后误差
-// float Sum_Error3 = 0, Prev_Error3 = 0;     //误差积分
-// float Gain3 = 1.2, PID_I_MAX3 = 100.0, PID_I_MIN3 = -100.0, V_DATA_MAX3 = 100, V_DATA_MIN3 = 0;
-
-void PID_calc(void);
-int Velocity_FeedbackControl(double TargetVelocity, double CurrentVelocity);
+int Velocity_FeedbackControl_1(double TargetVelocity, double CurrentVelocity);
+int Velocity_FeedbackControl_2(double TargetVelocity, double CurrentVelocity);
 
 #endif // PID_H_INCLUDED
--- STM32F401xC/HAL/100_Car/User/Src/pid.c
+++ STM32F401xC/HAL/100_Car/User/Src/pid.c
@@ -1,6 +1,6 @@
 #include "pid.h"
 
-double P = 0.8000, I = 0.0100, D = 300.0; // PID constants
+double P = 1.5000, I = 0.0050, D = 0.0; // PID constants
 
 u16 pwmval_1;                // 定时器PWM占空比设置
 u16 pwmval_2;                // 定时器PWM占空比设置
@@ -8,132 +8,6 @@ double Encoder_1;            // 当前1速度
 double Encoder_2;            // 当前2速度
 double TargetSpeed_1 = 0.25; // 目标速度
 double TargetSpeed_2 = 0.25; // 目标速度
-
-double PID_OUT = 0;                         // PID输出
-double P_OUT = 0, I_OUT = 0, D_OUT = 0;     //比例输出，积分输出，微分输出
-double Current_Error = 0, Last_Error = 0;   //当前误差  最后误差
-double Sum_Error = 0;                       //误差积分
-double PID_I_MAX = 10.0, PID_I_MIN = -10.0; // PID积分上限，PID积分下限
-
-void PID_calc(void) // PID算法
-{
-
-    // float Rate;                           //误差变化率
-    // Current_Error = Set_Temp - Encoder_1; //当前误差
-    // Sum_Error += Current_Error;           //误差积分
-    // Prev_Error = Last_Error;              //存储误差积分
-    // Last_Error = Current_Error;           //存储误差分析
-    // Rate = Current_Error - Last_Error;    //变化速率计算
-
-    // if (Rate > 10) //不让ta大于5也不让ta小于5
-    //     Rate = 10;
-    // if (Rate < -10)
-    //     Rate = -10;
-
-    // P_OUT = P * Gain * Current_Error; //比列项
-    // I_OUT = I * Gain * Sum_Error;     //积分项
-
-    // //积分限幅处理
-    // if (I_OUT > PID_I_MAX)
-    //     I_OUT = PID_I_MAX; //不能超过最大值不能低于最小值
-    // if (I_OUT < PID_I_MIN)
-    //     I_OUT = PID_I_MIN;
-
-    // //微分输出处理
-    // D_OUT = D * Gain * Rate;
-    // PID_OUT = P_OUT + I_OUT + D_OUT;
-    // if (PID_OUT >= V_DATA_MAX)
-    //     PID_OUT = V_DATA_MAX;
-    // if (PID_OUT <= V_DATA_MIN)
-    //     PID_OUT = V_DATA_MIN;
-    // /*******************************************************/
-    // float Rate1;                            //误差变化率
-    // Current_Error1 = Set_Temp1 - Encoder_2; //当前误差
-    // Sum_Error1 += Current_Error1;           //误差积分
-    // Prev_Error1 = Last_Error1;              //存储误差积分
-    // Last_Error1 = Current_Error1;           //存储误差分析
-    // Rate1 = Current_Error1 - Last_Error1;   //变化速率计算
-
-    // if (Rate1 > 10) //不让ta大于5也不让ta小于5
-    //     Rate1 = 10;
-    // if (Rate1 < -10)
-    //     Rate1 = -10;
-
-    // P_OUT1 = P * Gain1 * Current_Error1; //比列项
-    // I_OUT1 = I * Gain1 * Sum_Error1;     //积分项
-
-    // //积分限幅处理
-    // if (I_OUT1 > PID_I_MAX1)
-    //     I_OUT1 = PID_I_MAX1; //不能超过最大值不能低于最小值
-    // if (I_OUT1 < PID_I_MIN1)
-    //     I_OUT1 = PID_I_MIN1;
-
-    // //微分输出处理
-    // D_OUT1 = D * Gain * Rate1;
-    // PID_OUT1 = P_OUT1 + I_OUT1 + D_OUT1;
-    // if (PID_OUT1 >= V_DATA_MAX1)
-    //     PID_OUT1 = V_DATA_MAX1;
-    // if (PID_OUT1 <= V_DATA_MIN1)
-    //     PID_OUT1 = V_DATA_MIN1;
-    // /*******************************************************/
-    // float Rate2;                          //误差变化率
-    // Current_Error2 = Set_Temp2 - V_3;     //当前误差
-    // Sum_Error2 += Current_Error2;         //误差积分
-    // Prev_Error2 = Last_Error2;            //存储误差积分
-    // Last_Error2 = Current_Error2;         //存储误差分析
-    // Rate2 = Current_Error2 - Last_Error2; //变化速率计算
-
-    // if (Rate2 > 10) //不让ta大于5也不让ta小于5
-    //     Rate2 = 10;
-    // if (Rate2 < -10)
-    //     Rate2 = -10;
-
-    // P_OUT2 = P * Gain2 * Current_Error2; //比列项
-    // I_OUT2 = I * Gain2 * Sum_Error2;     //积分项
-
-    // //积分限幅处理
-    // if (I_OUT2 > PID_I_MAX2)
-    //     I_OUT2 = PID_I_MAX2; //不能超过最大值不能低于最小值
-    // if (I_OUT2 < PID_I_MIN2)
-    //     I_OUT2 = PID_I_MIN2;
-
-    // //微分输出处理
-    // D_OUT2 = D * Gain2 * Rate2;
-    // PID_OUT2 = P_OUT2 + I_OUT2 + D_OUT2;
-    // if (PID_OUT2 >= V_DATA_MAX2)
-    //     PID_OUT2 = V_DATA_MAX2;
-    // if (PID_OUT2 <= V_DATA_MIN2)
-    //     PID_OUT2 = V_DATA_MIN2;
-    // /*******************************************************/
-    // float Rate3;                          //误差变化率
-    // Current_Error3 = Set_Temp3 - V_4;     //当前误差
-    // Sum_Error3 += Current_Error3;         //误差积分
-    // Prev_Error3 = Last_Error3;            //存储误差积分
-    // Last_Error3 = Current_Error3;         //存储误差分析
-    // Rate3 = Current_Error3 - Last_Error3; //变化速率计算
-
-    // if (Rate3 > 10) //不让ta大于5也不让ta小于5
-    //     Rate3 = 10;
-    // if (Rate3 < -10)
-    //     Rate3 = -10;
-
-    // P_OUT3 = P * Gain3 * Current_Error3; //比列项
-    // I_OUT3 = I * Gain3 * Sum_Error3;     //积分项
-
-    // //积分限幅处理
-    // if (I_OUT3 > PID_I_MAX3)
-    //     I_OUT3 = PID_I_MAX3; //不能超过最大值不能低于最小值
-    // if (I_OUT3 < PID_I_MIN3)
-    //     I_OUT3 = PID_I_MIN3;
-
-    // //微分输出处理
-    // D_OUT3 = D * Gain3 * Rate3;
-    // PID_OUT3 = P_OUT3 + I_OUT3 + D_OUT3;
-    // if (PID_OUT3 >= V_DATA_MAX3)
-    //     PID_OUT3 = V_DATA_MAX3;
-    // if (PID_OUT3 <= V_DATA_MIN3)
-    //     PID_OUT3 = V_DATA_MIN3;
-}
 
 /**************************************************************************
 函数功能：速度闭环PID控制(实际为PI控制)
@@ -147,10 +21,15 @@ ControlVelocity代表增量输出
 在我们的速度控制闭环系统里面，只使用PI控制
 ControlVelocity+=Kp[e（k）-e(k-1)]+Ki*e(k)
 **************************************************************************/
-int Velocity_FeedbackControl(double TargetVelocity, double CurrentVelocity)
+int Velocity_FeedbackControl_1(double TargetVelocity, double CurrentVelocity)
 {
-    double Rate;                   // 定义相关变量
-    static double ControlVelocity; // 定义控制输出
+    double Rate;                                                                          // 定义相关变量
+    static double ControlVelocity;                                                        // 定义控制输出
+    static double PID_OUT = 0;                                                            // PID输出
+    static double P_OUT = 0, I_OUT = 0, D_OUT = 0;                                        //比例输出，积分输出，微分输出
+    static double Current_Error = 0, Last_Error = 0;                                      //当前误差  最后误差
+    static double Sum_Error = 0;                                                          //误差积分
+    static double PID_I_MAX = 5.0, PID_I_MIN = -5.0, V_DATA_MAX = 20, V_DATA_MIN = -20; // PID积分上限，PID积分下限
 
     Current_Error = TargetVelocity - CurrentVelocity; //求速度偏差
     P_OUT = P * Current_Error;                        //比列项
@@ -171,14 +50,69 @@ int Velocity_FeedbackControl(double TargetVelocity, double CurrentVelocity)
     D_OUT = D * Rate;                  // 微分输出
 
     PID_OUT = P_OUT + I_OUT + D_OUT; // PID输出
-    // if (PID_OUT >= V_DATA_MAX)       // 如果输出大于最大值，则输出最大值
-    // {
-    //     PID_OUT = V_DATA_MAX;
-    // }
-    // if (PID_OUT <= V_DATA_MIN) // 如果输出小于最小值，则输出最小值
-    // {
-    //     PID_OUT = V_DATA_MIN;
-    // }
+    if (PID_OUT >= V_DATA_MAX)       // 如果输出大于最大值，则输出最大值
+    {
+        PID_OUT = V_DATA_MAX;
+    }
+    if (PID_OUT <= V_DATA_MIN) // 如果输出小于最小值，则输出最小值
+    {
+        PID_OUT = V_DATA_MIN;
+    }
+
+    ControlVelocity += PID_OUT; // PID输出
+
+    return ControlVelocity; //返回速度控制值
+}
+
+/**************************************************************************
+函数功能：速度闭环PID控制(实际为PI控制)
+入口参数：目标速度 当前速度
+返回  值：速度控制值
+根据增量式离散PID公式
+ControlVelocity+=Kp[e（k）-e(k-1)]+Ki*e(k)+Kd[e(k)-2e(k-1)+e(k-2)]
+e(k)代表本次偏差
+e(k-1)代表上一次的偏差  以此类推
+ControlVelocity代表增量输出
+在我们的速度控制闭环系统里面，只使用PI控制
+ControlVelocity+=Kp[e（k）-e(k-1)]+Ki*e(k)
+**************************************************************************/
+int Velocity_FeedbackControl_2(double TargetVelocity, double CurrentVelocity)
+{
+    double Rate;                                                                          // 定义相关变量
+    static double ControlVelocity;                                                        // 定义控制输出
+    static double PID_OUT = 0;                                                            // PID输出
+    static double P_OUT = 0, I_OUT = 0, D_OUT = 0;                                        //比例输出，积分输出，微分输出
+    static double Current_Error = 0, Last_Error = 0;                                      //当前误差  最后误差
+    static double Sum_Error = 0;                                                          //误差积分
+    static double PID_I_MAX = 5.0, PID_I_MIN = -5.0, V_DATA_MAX = 20, V_DATA_MIN = -20; // PID积分上限，PID积分下限
+
+    Current_Error = TargetVelocity - CurrentVelocity; //求速度偏差
+    P_OUT = P * Current_Error;                        //比列项
+
+    Sum_Error += Current_Error; //误差积分
+    I_OUT = I * Sum_Error;      //积分项
+    if (I_OUT > PID_I_MAX)      //积分限幅处理,不能超过最大值不能低于最小值
+    {
+        I_OUT = PID_I_MAX;
+    }
+    if (I_OUT < PID_I_MIN)
+    {
+        I_OUT = PID_I_MIN;
+    }
+
+    Rate = Current_Error - Last_Error; //变化速率计算
+    Last_Error = Current_Error;        //存储误差分析
+    D_OUT = D * Rate;                  // 微分输出
+
+    PID_OUT = P_OUT + I_OUT + D_OUT; // PID输出
+    if (PID_OUT >= V_DATA_MAX)       // 如果输出大于最大值，则输出最大值
+    {
+        PID_OUT = V_DATA_MAX;
+    }
+    if (PID_OUT <= V_DATA_MIN) // 如果输出小于最小值，则输出最小值
+    {
+        PID_OUT = V_DATA_MIN;
+    }
 
     ControlVelocity += PID_OUT; // PID输出
 
