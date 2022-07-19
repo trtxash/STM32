914ee897df003a893e205db9a10b4b47134561e9
TRTX-gamer <w1165577077@qq.com>
2022-07-19 21:30:15 +0800

    创建新模板

--- STM32F401xC/HAL/00_Template/Core/Src/main.c
+++ STM32F401xC/HAL/00_Template/Core/Src/main.c
@@ -1,33 +1,34 @@
 /**
- * @file	00_Template
- * @brief 	模板
+ * @file	100_Car
+ * @brief 	累
  * @author 	TRTX-gamer
  * @version 1.00
- * @date 	2022年7月12号14点04分
+ * @date 	2022年7月14号15点23分
  */
 
-#include "delay.h"
-#include "sys.h"
-#include "led.h"
-#include "stm32f4xx.h"
+#include "main.h"
 
 /**
- * @brief	LED灯交替闪烁
+ * @brief	主函数,程序入口
  * @param 	none
  * @arg		none
- * @note  	初始化函数后利用LED_Reverse以达到LED灯交替闪烁
+ * @note  	初始化函数后利用定时器5PWM发生器实现4路PWM发生器
  * @retval	int
  */
 int main(void)
 {
-	HAL_Init(); //初始化HAL库
+	u16 arr = 500 - 1;
+	u16 psc = 84 - 1;
+
+	HAL_Init();
 	Stm32_Clock_Init(168, 25, 2, 4);
 	delay_init(84); //初始化延时函数
-	LED_Init();		//初始化LED
-
+	OLED_Init();
+	TIM2_Init(10000 - 1, 84 - 1);	 // 100Hz刷新OLED
+	TIM5_PWM_Init(arr, psc, 0B1111); // 2kHz，50%，4路,84M/84=1M的计数频率，自动重装载为500，那么PWM频率为1M/500=2kHZ
+	Encoder_Init();					 // 初始化电机编码器
+	OLED_Display();					 //显示初始化信息
 	while (1)
 	{
-		LED_Reverse();
-		delay_ms(500); //延时500ms
 	}
 }
