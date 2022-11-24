fd1bee929333a61815a8d865e71b5714ac02c227
TRTX-gamer <w1165577077@qq.com>
2022-11-24 19:20:38 +0800

    new

---
+++ STM32F407xG/HAL/05_FreeRTOS_MPU6050/Core/Inc/pid.h
@@ -0,0 +1,13 @@
+#ifndef _PID_H
+#define _PID_H
+
+#include "sys.h"
+
+int Velocity_FeedbackControl_1(double TargetVelocity, double CurrentVelocity);
+int Velocity_FeedbackControl_2(double TargetVelocity, double CurrentVelocity);
+int Velocity_FeedbackControl_3(double TargetVelocity, double CurrentVelocity);
+int Velocity_FeedbackControl_4(double TargetVelocity, double CurrentVelocity);
+int X_FeedbackControl_1(double TargetVelocity, double CurrentVelocity);
+int X_FeedbackControl_2(double TargetVelocity, double CurrentVelocity);
+
+#endif // PID_H_INCLUDED
