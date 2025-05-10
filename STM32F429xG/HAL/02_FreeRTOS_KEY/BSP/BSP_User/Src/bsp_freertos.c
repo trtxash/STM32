6c259679980ea0548a8fa73961718e3b8e9cbbdd
“trtxash” <w1165577077@qq.com>
2025-05-10 19:22:39 +0800

    refactor: 优化key_task和test_task逻辑

--- STM32F429xG/HAL/02_FreeRTOS_KEY/BSP/BSP_User/Src/bsp_freertos.c
+++ STM32F429xG/HAL/02_FreeRTOS_KEY/BSP/BSP_User/Src/bsp_freertos.c
@@ -3,7 +3,6 @@
 #include "led.h"
 #include "log_rtt.h"
 #include "sdram.h"
-#include <stdint.h>
 
 u8 keyval = 0;
 
@@ -27,6 +26,11 @@ void key_task(void);          // 任务函数
 #define TEST_TSTK_SIZE 256 + 1 // 任务堆栈大小
 TaskHandle_t TESTTask_Handler; // 任务句柄
 void test_task(void);          // 任务函数
+
+// #define TEST_TASK_PRIO 11      // 任务优先级,越大越高优先级
+// #define TEST_TSTK_SIZE 256 + 1 // 任务堆栈大小
+// TaskHandle_t TESTTask_Handler; // 任务句柄
+// void test_task(void);          // 任务函数
 
 void freertos_main(void)
 {
@@ -61,13 +65,13 @@ void start_task(void)
                 (UBaseType_t)KEY_TASK_PRIO,
                 (TaskHandle_t *)&KEYTask_Handler);
 
-    // // 创建test任务
-    // xTaskCreate((TaskFunction_t)test_task,
-    //             (const char *)"test_task",
-    //             (uint16_t)TEST_TSTK_SIZE,
-    //             (void *)NULL,
-    //             (UBaseType_t)TEST_TASK_PRIO,
-    //             (TaskHandle_t *)&TESTTask_Handler);
+    // 创建test任务
+    xTaskCreate((TaskFunction_t)test_task,
+                (const char *)"test_task",
+                (uint16_t)TEST_TSTK_SIZE,
+                (void *)NULL,
+                (UBaseType_t)TEST_TASK_PRIO,
+                (TaskHandle_t *)&TESTTask_Handler);
 
     vTaskDelete(StartTask_Handler); // 删除开始任务
     taskEXIT_CRITICAL();            // 退出临界区
@@ -88,42 +92,57 @@ void led_task(void)
 // key任务函数
 void key_task(void)
 {
-    uint32_t i = 0;
+    // uint32_t i = 0;
     TickType_t xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
     while (1)
     {
+
         KEY_Scan(&KEY_UP_DATA, KEY_UP_READ());
+        // if (KEY_UP_DATA.K_value)
+        // {
+        //     i++;
+        //     KEY_UP_DATA.K_value = 0;
+        //     LOGI("KEY_UP,i=%u", i);
+        // }
         if (KEY_UP_DATA.K_value)
         {
-            i++;
+            static uint8_t flag = 1;
+
             KEY_UP_DATA.K_value = 0;
-            LOGI("KEY_UP,i=%u", i);
+            if (flag)
+            {
+                LED2_Clr();
+                vTaskSuspend(TESTTask_Handler);
+                flag = !flag;
+            }
+            else
+            {
+                LED2_Set();
+                vTaskResume(TESTTask_Handler);
+                flag = !flag;
+            }
         }
-        // if (KEY_BOOT_DATA.K_value)
-        // {
-        //     LOGI("KEY_BOOT");
-        // }
         vTaskDelayUntil(&xLastWakeTime, 50);
     }
 }
 
-// // 测试任务函数
-// void test_task(void)
-// {
-//     static uint8_t flag = 0;
-//     TickType_t xLastWakeTime;
-//     xLastWakeTime = xTaskGetTickCount();
-//     while (1)
-//     {
-//         flag = !flag;
-//         LED1_Reverse();
-//         // taskENTER_CRITICAL(); // 进入临界区
-//         if (flag)
-//             SDRAM_WriteSpeedTest_32bits();
-//         else
-//             SDRAM_ReadSpeedTest();
-//         // taskEXIT_CRITICAL(); // 退出临界区
-//         vTaskDelayUntil(&xLastWakeTime, 1000);
-//     }
-// }
+// 测试任务函数
+void test_task(void)
+{
+    static uint8_t flag = 0;
+    TickType_t xLastWakeTime;
+    xLastWakeTime = xTaskGetTickCount();
+    while (1)
+    {
+        flag = !flag;
+        LED1_Reverse();
+        // taskENTER_CRITICAL(); // 进入临界区
+        if (flag)
+            SDRAM_WriteSpeedTest_32bits();
+        else
+            SDRAM_ReadSpeedTest();
+        // taskEXIT_CRITICAL(); // 退出临界区
+        vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+}
