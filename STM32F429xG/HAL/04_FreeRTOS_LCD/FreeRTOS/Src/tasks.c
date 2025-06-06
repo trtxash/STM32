6c23fd0af65bb917d1d31c05892b2dac523bb1e3
“trtxash” <w1165577077@qq.com>
2025-06-06 16:40:12 +0800

    fix(tasks.c): 调整运行时统计信息的格式
    将运行时统计信息格式从原有的制表符分隔调整为固定长度的格式，以提高输出的整齐性和可读性。
    - 修改了 `vTaskGetRunTimeStats` 函数中 `ulStatsAsPercentage` 大于1%时的 `sprintf` 调用，添加了 `%10u` 来确保运行时间计数器的输出长度为10。
    - 同样修改了 `ulStatsAsPercentage` 小于1%时的 `sprintf` 调用，添加了 `%10u` 来确保运行时间计数器的输出长度为10，同时保留 `<1%` 格式。

--- STM32F429xG/HAL/04_FreeRTOS_LCD/FreeRTOS/Src/tasks.c
+++ STM32F429xG/HAL/04_FreeRTOS_LCD/FreeRTOS/Src/tasks.c
@@ -4611,7 +4611,8 @@ void vTaskGetRunTimeStats(char *pcWriteBuffer)
                     {
                         /* sizeof( int ) == sizeof( long ) so a smaller
                          * printf() library can be used. */
-                        sprintf(pcWriteBuffer, "\t%u\t\t%u%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter, (unsigned int)ulStatsAsPercentage); /*lint !e586 sprintf() allowed as this is compiled with many compilers and this is a utility function only - not part of the core kernel implementation. */
+                        // sprintf(pcWriteBuffer, "\t%u\t\t%u%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter, (unsigned int)ulStatsAsPercentage); /*lint !e586 sprintf() allowed as this is compiled with many compilers and this is a utility function only - not part of the core kernel implementation. */
+                        sprintf(pcWriteBuffer, "%10u\t%2u%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter, (unsigned int)ulStatsAsPercentage); // 定长10
                     }
 #endif
                 }
@@ -4627,7 +4628,8 @@ void vTaskGetRunTimeStats(char *pcWriteBuffer)
                     {
                         /* sizeof( int ) == sizeof( long ) so a smaller
                          * printf() library can be used. */
-                        sprintf(pcWriteBuffer, "\t%u\t\t<1%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter); /*lint !e586 sprintf() allowed as this is compiled with many compilers and this is a utility function only - not part of the core kernel implementation. */
+                        // sprintf(pcWriteBuffer, "\t%u\t\t<1%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter); /*lint !e586 sprintf() allowed as this is compiled with many compilers and this is a utility function only - not part of the core kernel implementation. */
+                        sprintf(pcWriteBuffer, "%10u\t<1%%\r\n", (unsigned int)pxTaskStatusArray[x].ulRunTimeCounter); // 定长10
                     }
 #endif
                 }
